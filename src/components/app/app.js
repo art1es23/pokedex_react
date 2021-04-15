80de49b39e57a5376bcc0991516503aebe9cdd39
Denys Symak <dreamwinamounts@gmail.com>
2021-04-15 22:50:26 +0300

    init

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './app.css';
+
+const App = () => {
+    return (
+        <>
+            <h1>HELLOOOOOOOOOO</h1>
+        </>
+    )
+}
+
+export default App;
